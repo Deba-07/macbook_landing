75cd9372ef84169dd6e26e5ec5d36182d960df3e
Deba-07 <dynamicdeba07@gmail.com>
2025-10-28 11:09:40 +0530

    feat: add GSAP and React Three dependencies; integrate ProductViewer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import gsap from 'gsap'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import ProductViewer from './components/ProductViewer'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import { Scroll } from '@react-three/drei'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
